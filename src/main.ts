52ca1bb2208eccc898303ab22670155eca68a5b9
yeminicon <akinyele.adeyemi9005@gmail.com>
2023-09-16 17:16:43 +0100

    first repo

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'https://deepisces.com.ng/',
-    // origin: 'http://localhost:3000',
+    origin: ['https://deepisces.com.ng', 'http://localhost:3000'], // Specify multiple origins in an array
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
   await app.listen(3001);
 }
+
 bootstrap();
